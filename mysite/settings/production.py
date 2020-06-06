96e3c5157fc6ac6d9d606e3d21dac4662d091dd9
Thibaud <thibaudcolas@gmail.com>
2020-06-06 19:36:30 +0100

    heroku configuration

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,8 +1,21 @@
+import os
+import dj_database_url
 from .base import *
 
+
+env = os.environ.copy()
+
+SECRET_KEY = env['SECRET_KEY']
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 DEBUG = False
 
 try:
-    from .local import *
+   from .local import *
 except ImportError:
-    pass
+   pass
