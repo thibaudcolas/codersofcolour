3abe121ee7b3e671a976fe6e4a9d8551f6860fc2
Thibaud <thibaudcolas@gmail.com>
2020-06-06 21:23:32 +0100

    Added Whitenoise

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -15,6 +15,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 try:
    from .local import *
 except ImportError:
