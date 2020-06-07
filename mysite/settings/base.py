f1bf0186f82a77405e0ce64d59ed8e08b4f72194
Thibaud <thibaudcolas@gmail.com>
2020-06-07 19:15:48 +0100

    Add S3 settings

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -162,3 +162,16 @@ WAGTAIL_SITE_NAME = "mysite"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+env = os.environ.copy() # Add this near the top after the imports
+
+
+if "AWS_STORAGE_BUCKET_NAME" in env:
+    INSTALLED_APPS.append("storages")
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_STORAGE_BUCKET_NAME = env["AWS_STORAGE_BUCKET_NAME"]
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
+    if "AWS_S3_CUSTOM_DOMAIN" in env:
+        AWS_S3_CUSTOM_DOMAIN = env["AWS_S3_CUSTOM_DOMAIN"]
+        AWS_S3_URL_PROTOCOL = env.get("AWS_S3_URL_PROTOCOL", "https:")
